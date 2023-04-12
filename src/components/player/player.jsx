89f4b213f1ba19065e7d8f3ba05adeefe846f482
rqphy <raphael.ferreira@hetic.net>
2023-04-12 11:28:18 +0200

    :dizzy: player > add scale parallax animation on imgs

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,15 +1,29 @@
+import { useRef } from "react"
+import { motion, useScroll, useTransform } from "framer-motion"
+
+function useParallax(value, scale) {
+    return useTransform(value, [0.5, 1], [1, scale])
+}
+
 export default function Player({ id, player }) {
-	return (
-		<article className="player">
-			<h2 className="player__title">
-				{10 - id} - {player.name}
-			</h2>
-			<img
-				className="player__illu"
-				src={player.imgURL}
-				alt={player.name}
-			/>
-			<p className="player__history">{player.history}</p>
-		</article>
-	)
+    const txtRef = useRef(null)
+    const { scrollYProgress } = useScroll({ target: txtRef })
+    const scale = useParallax(scrollYProgress, 1.2)
+
+    return (
+        <article className="player">
+            <h2 ref={txtRef} className="player__title">
+                {10 - id} - {player.name}
+            </h2>
+            <figure className="player__ctn">
+                <motion.img
+                    style={{ scale }}
+                    className="player__illu"
+                    src={player.imgURL}
+                    alt={player.name}
+                />
+            </figure>
+            <p className="player__history">{player.history}</p>
+        </article>
+    )
 }
